fadda9f74df6a2789413b5da8604864db93e0967
zyb <1140114702@qq.com>
2017-06-27 15:49:01 +0800

    Initial commit

---
+++ app/src/main/java/com/zwonb/chapter3/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zwonb.chapter3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startMain2(View view) {
+        startActivity(new Intent(this, Main2Activity.class));
+    }
+}
---
+++ app/src/main/java/com/zwonb/chapter3/ScrollerTest.java
@@ -0,0 +1,51 @@
+package com.zwonb.chapter3;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.Scroller;
+import android.widget.TextView;
+
+/**
+ * Created by zyb on 2017/6/26.
+ */
+
+public class ScrollerTest extends TextView {
+
+    private Scroller mScroller = new Scroller(getContext());
+
+    public ScrollerTest(Context context) {
+        super(context);
+    }
+
+    public ScrollerTest(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScrollerTest(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void smoothScrollTo(int destX, int destY) {
+        int scrollX = getScrollX();
+        int deltaX = destX - scrollX;
+        mScroller.startScroll(scrollX, 0, deltaX, 0, 1000);
+        invalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        smoothScrollTo(100,40);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            postInvalidate();
+        }
+    }
+}
